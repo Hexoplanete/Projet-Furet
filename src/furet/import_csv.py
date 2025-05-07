040d61d8fe5315d1bd259515525ca6928c459771
Orlando <orlando@Badlands.local>
2025-05-07 09:28:47 +0200

    datetimeImportCSV

--- src/furet/import_csv.py
+++ src/furet/import_csv.py
@@ -1,19 +1,23 @@
 import csv
+from datetime import datetime
+from datetime import date
+
 path = "./Database/57/"
 
 """
 """
 
 class Arrete:
-    def __init__(self, typeDocument, campagneAspas, departement, numArrete, titreArrete, dateSignatureArrete, numRaa, datePubliRaa, pages, statutTraitement, commentaire):
+    def __init__(self, typeDocument, campagneAspas, departement, numArrete, titreArrete, dateSignatureArrete, numRaa, datePubliRaa, url, pages, statutTraitement, commentaire):
         self.typeDocument = typeDocument
         self.campagneAspas = campagneAspas
         self.departement = departement
         self.numArrete = numArrete
         self.titreArrete = titreArrete
-        self.dateSignatureArrete = dateSignatureArrete
+        self.dateSignatureArrete = datetime.strptime(dateSignatureArrete, format).date()
         self.numRaa = numRaa
-        self.datePubliRaa = datePubliRaa
+        self.datePubliRaa = datetime.strptime(datePubliRaa, format).date()
+        self.url = url
         self.pages = pages
         self.statutTraitement = statutTraitement
         self.commentaire = commentaire
@@ -25,14 +29,22 @@ if __name__ == "__main__":
 
 #lecture d'un fichier 
     with open(path + '57_2025_04_RAA.csv', encoding='utf-8') as file:
+
         reader = csv.reader(file, delimiter=',')
+
+        # On recupere le header separément du reste des lignes sinon ca pose probleme pour le cast
+        header = next(reader)
 
         l = []
         for row in reader:
-            print(row)
-            #aa = Arrete("Alice", 30)
-            #print(aa)
-
-            #l.append(aa)
-        #filtrer
-
+            try: 
+                aa = Arrete(
+                    row[0], row[1], row[2], row[3], row[4], row[5], 
+                    row[6], row[7], row[8], row[9], row[10], row[11]
+                )
+                
+                l.append(aa)
+    
+            except Exception as e:
+                print(f"Erreur de parsing, ligne ignorée : {row}")
+                print(f"Erreur : {e}")

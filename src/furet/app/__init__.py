2cd1fab6c5c55aee65523b19f153f033f227b77e
Spiky-73 <cocojeanne73@gmail.com>
2025-05-09 15:55:39 +0200

    set app name

--- src/furet/app/__init__.py
+++ src/furet/app/__init__.py
@@ -6,13 +6,15 @@ from .windows.decreeTableWindow import DecreeTableWindow
 
 def main():
     app = QApplication(sys.argv)
-    if os.path.isdir("/usr/lib/x86_64-linux-gnu/qt6/plugins/"):
-        app.addLibraryPath("/usr/lib/x86_64-linux-gnu/qt6/plugins/")
+    app.setApplicationName("Fouille Universelle de Recueils pour Entreposage et Traitement")
+    app.setOrganizationDomain("github.com/Hexoplanete/Projet-Furet/")
+    app.setOrganizationName("Hexoplanète")
+    if os.path.isdir("/usr/lib/x86_64-linux-gnu/qt6/plugins"):
+        app.addLibraryPath("/usr/lib/x86_64-linux-gnu/qt6/plugins")
         if "Breeze" in QStyleFactory.keys():
             app.setStyle("Breeze")
         elif "kvantum" in QStyleFactory.keys():
             app.setStyle("kvantum")
-    # app.setStyle("Windows")
     window = DecreeTableWindow()
     window.showMaximized()
     return app.exec()

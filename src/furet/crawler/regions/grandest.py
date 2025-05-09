a2d6e9f16dfdba20157e35a8f5b54ef0fa546eea
Ereguof <v0.rddos@gmail.com>
2025-05-09 09:27:49 +0200

    added aube

--- src/furet/crawler/regions/grandest.py
+++ src/furet/crawler/regions/grandest.py
@@ -106,3 +106,110 @@ class Moselle(Spider):
             return None
         
         return finalLinks
+    
+
+class Aube(Spider):
+    """
+    A spider class for crawling the Ariege department's website for RAA (Recueil des Actes Administratifs) links.
+    Inherits from the Spider class.
+    """
+    def __init__(self, outputDir, configFile, linkFile, date):
+        """
+        Initialize the Ariege spider with specific parameters.
+        """
+        super().__init__(outputDir, configFile, linkFile, date)
+        self.baseUrl = "https://www.aube.gouv.fr/Publications/RAA-Recueil-des-Actes-Administratifs"
+        self.region = "GrandEst"
+        self.department = "Aube"
+        self.currentMostRecentRAA = self.mostRecentRAA
+
+    def findPages(self, html):
+        """
+        Find the number of pages available in the HTML content.
+
+        :param html: HTML content of a page.
+        :return: Number of pages found.
+        """
+        extractedPages = []
+        extractedPagesFinal = []
+        soup = BeautifulSoup(html, 'html.parser') 
+        
+        div = soup.find('div', class_='fr-text--lead fr-my-3w')
+        aList = div.find_all('a', href=True, class_='fr-link')
+
+        for a in aList:
+            if a['href'].startswith('/Publications/RAA-Recueil-des-Actes-Administratifs/RAA'):
+                annee = a.text.split()[-1] # Extract the year from the link
+                if int(annee) < self.mostRecentRAA.year: # Check if the year is less than the most recent RAA year for the optimization. We can stop the loop here earlier.
+                    continue
+                extractedPages.append('https://www.aube.gouv.fr' + a['href']) 
+
+        for link in extractedPages:
+            i = 0
+            while True:
+                url = link + "/(offset)/" + str(i*10) # Pagination URL the value of offset is multiplied by 10 to get the next page
+                
+                html = self.fetchPage(url)
+                soup = BeautifulSoup(html, 'html.parser')
+                i += 1
+
+                if not html or not soup.find(class_="fr-card__link menu-item-link"):
+                    break
+
+                extractedPagesFinal.append(url)
+
+        return extractedPagesFinal
+
+    def extractLinks(self, html, links):
+        """
+        Extract all links from the HTML content.
+
+        :param html: HTML content of a page.
+        :return: List of extracted links.
+        """
+        soup = BeautifulSoup(html, 'html.parser')
+        rows = soup.find_all('a', href=True, class_='fr-card__link menu-item-link')
+
+        for row in rows:
+            try:
+                if not row['href'].startswith('/contenu/telechargement'):
+                    continue
+                dateStr = row["title"].split()[-1] # Extract the date from the title attribute
+                date = datetime.strptime(dateStr, "%d/%m/%Y")
+
+                if date > self.mostRecentRAA:
+                    link = row['href']
+                    links.append({"link": 'https://www.aube.gouv.fr' + link, "datePublication": dateStr, "region": self.region, "department": self.department}) # Add the link to the list for the JSON file
+                    if date > self.currentMostRecentRAA:
+                        self.currentMostRecentRAA = date
+
+            except (ValueError, IndexError) as e:
+                print(f"Error parsing row: {row}, Error: {e}")
+                continue
+
+        return links
+
+    def crawl(self):
+        try:
+            linksPages = self.findPages(self.fetchPage(self.baseUrl)) 
+            links = []
+            for link in linksPages:
+               
+                html = self.fetchPage(link)
+                if not html:
+                    break
+
+                self.extractLinks(html, links)
+            # Update the most recent RAA if a newer one is found
+            if self.currentMostRecentRAA > self.mostRecentRAA: 
+                self.mostRecentRAA = self.currentMostRecentRAA
+                self.setMostRecentRAADate(self.mostRecentRAA, self.region, self.department)
+
+            self.addToJsonResultFile(links)
+
+        except Exception as e:
+            print(f"Error during crawling: {e}")
+            return None
+        
+        return links
+    

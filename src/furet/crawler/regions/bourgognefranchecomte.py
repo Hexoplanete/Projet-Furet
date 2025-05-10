b09ac1a2246e45ac1588150577f79a33f8a22855
amadousow <152507665+asowww@users.noreply.github.com>
2025-05-10 18:08:53 +0200

    added 5 departments

--- src/furet/crawler/regions/bourgognefranchecomte.py
+++ src/furet/crawler/regions/bourgognefranchecomte.py
@@ -94,6 +94,75 @@ class SaoneEtLoire(Spider):
         
         return links
     
+class Doubs(Spider):
+    """
+    A spider class for crawling the Doubs department's website for RAA (Recueil des Actes Administratifs) links.
+    Inherits from the Spider class.
+    """
+    def __init__(self, outputDir, configFile, linkFile, date):
+        """
+        Initialize the Doubs spider with specific parameters.
+        """
+        super().__init__(outputDir, configFile, linkFile, date)
+        self.baseUrl = "https://www.doubs.gouv.fr/Publications/Publications-Legales/Recueil-des-Actes-Administratifs-RAA"
+        self.region = "BourgogneFrancheComte"
+        self.department = "Doubs"
+        self.currentMostRecentRAA = self.mostRecentRAA
+
+    def findPages(self, html):
+        """
+        Find the number of pages available in the HTML content.
+
+        :param html: HTML content of a page.
+        :return: Number of pages found.
+        """
+        extractedPages = []
+        i = 0
+        while True:
+            url = self.baseUrl+ "/(offset)/" + str(i*10) # Pagination URL the value of offset is multiplied by 10 to get the next page
+            
+            html = self.fetchPage(url)
+            soup = BeautifulSoup(html, 'html.parser')
+            if not html or not soup.find(class_="fr-card__link"):
+                break
+            i += 1
+
+            RAAYears = soup.find_all('a', class_='fr-card__link')
+            for RAAYear in RAAYears:
+                if RAAYear['href'].startswith('/Publications/Publications-Legales/Recueil-des-Actes-Administratifs-RAA/Recueil-des-actes-administratifs-pour-le-Doubs'):
+                    annee = RAAYear['href'].split('-')[-1]
+                    if int(annee[-4:]) < self.mostRecentRAA.year: # Check if the year is less than the most recent RAA year for the optimization. We can stop the loop here earlier.
+                        break
+                            
+                    extractedPages.append("https://www.doubs.gouv.fr" + RAAYear['href'])
+        return extractedPages
+
+    def extractLinks(self, html, links):
+        """
+        Extract all links from the HTML content.
+
+        :param html: HTML content of a page.
+        :return: List of extracted links.
+        """
+        soup = BeautifulSoup(html, 'html.parser')
+        rows = soup.find_all('a', href= True, class_="fr-link fr-link--download")
+
+        for row in rows:
+            try:
+                dateStr = row.find('span', class_='fr-link__detail').text.split()[-1] # Extract the date from the title attribute
+                date = datetime.strptime(dateStr, "%d/%m/%Y")
+
+                if date > self.mostRecentRAA:
+                    link = row['href']
+                    links.append({"link": 'https://www.doubs.gouv.fr' + link, "datePublication": dateStr, "region": self.region, "department": self.department}) # Add the link to the list for the JSON file
+                    if date > self.currentMostRecentRAA:
+                        self.currentMostRecentRAA = date
+
+            except (ValueError, IndexError) as e:
+                print(f"Error parsing row: {row}, Error: {e}")
+                continue
+
+        return links
 
 
 

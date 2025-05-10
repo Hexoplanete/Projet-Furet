74185bfccf5d8bbe8bac0a0765d5847c5de84216
amadousow <152507665+asowww@users.noreply.github.com>
2025-05-10 18:08:53 +0200

    added 5 departments

--- src/furet/crawler/regions/bretagne.py
+++ src/furet/crawler/regions/bretagne.py
@@ -85,3 +85,86 @@ class CotesDArmor(Spider):
                 continue
 
         return links
+    
+class Morbihan(Spider):
+    """
+    A spider class for crawling the Morbihan department's website for RAA (Recueil des Actes Administratifs) links.
+    Inherits from the Spider class.
+    """
+    def __init__(self, outputDir, configFile, linkFile, date):
+        """
+        Initialize the Morbihan spider with specific parameters.
+        """
+        super().__init__(outputDir, configFile, linkFile, date)
+        self.baseUrl = "https://www.morbihan.gouv.fr/RAA"
+        self.region = "Bretagne"
+        self.department = "Morbihan"
+        self.currentMostRecentRAA = self.mostRecentRAA
+
+    def findPages(self, html):
+        """
+        Find the number of pages available in the HTML content.
+
+        :param html: HTML content of a page.
+        :return: Number of pages found.
+        """
+        extractedPages = []
+        extractedPagesFinal = []
+        url = self.baseUrl
+        
+        html = self.fetchPage(url)
+        soup = BeautifulSoup(html, 'html.parser')
+
+        h2List = soup.find_all('h2', class_='fr-card__title')
+        for h2 in h2List:
+            a = h2.find('a', href=True)
+            if a['href'].startswith('/RAA/Annee'):
+                annee = a['href'].split('-')[-1]
+                if int(annee) < self.mostRecentRAA.year:
+                    break    
+                extractedPages.append(a['href'])
+
+
+        for link in extractedPages:
+            i = 0
+            while True:
+                url = "https://www.morbihan.gouv.fr" + link + "/(offset)/" + str(i*10) # Pagination URL the value of offset is multiplied by 10 to get the next page
+                
+                html = self.fetchPage(url)
+                soup = BeautifulSoup(html, 'html.parser')
+                i += 1
+
+                if not html or not soup.find(class_="fr-card__link menu-item-link"):
+                    break
+
+                extractedPagesFinal.append(url)
+
+        return extractedPagesFinal
+    
+
+    def extractLinks(self, html, links):
+        """
+        Extract all links from the HTML content.
+
+        :param html: HTML content of a page.
+        :return: List of extracted links.
+        """
+        soup = BeautifulSoup(html, 'html.parser')
+        rows = soup.find_all('a', href= True, class_='fr-card__link menu-item-link')
+
+        for row in rows:
+            try:
+                dateStr = row["title"].split()[-1] # Extract the date from the title attribute
+                date = datetime.strptime(dateStr, "%d/%m/%Y")
+
+                if date > self.mostRecentRAA:
+                    link = row['href']
+                    links.append({"link": 'https://www.morbihan.gouv.fr' + link, "datePublication": dateStr, "region": self.region, "department": self.department}) # Add the link to the list for the JSON file
+                    if date > self.currentMostRecentRAA:
+                        self.currentMostRecentRAA = date
+
+            except (ValueError, IndexError) as e:
+                print(f"Error parsing row: {row}, Error: {e}")
+                continue
+
+        return links

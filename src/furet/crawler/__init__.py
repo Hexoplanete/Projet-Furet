fe2e1cd3cd8e9cba7efc111d34dfaec71a61d875
tlevrard <thomaslevrardw@gmail.com>
2025-05-14 15:56:11 +0200

    work-with-crawler

--- src/furet/crawler/__init__.py
+++ src/furet/crawler/__init__.py
@@ -7,11 +7,13 @@ from furet.processing.processing import Processing
 
 
 def setup():
-    settings.setDefaultValue("crawler.autorun", False)
+    #settings.setDefaultValue("crawler.autorun", True) # Là on force à True si on veut le Crawler
+    settings.setDefaultValue("crawler.autorun", False) # Là on force à True si on veut le Crawler
     
+    # autorun is defined in a .config file, if it is False then no crawler (by default in the .config it is false)
     autorun = settings.value("crawler.autorun")
     if autorun:
-        from furet.processing.processing import Traitement
+        from furet.processing.processing import Processing
         start_time = time.time()
         crawler = Crawler()
         crawler_thread = threading.Thread(target=crawler.startCrawler)
@@ -27,8 +29,8 @@ def setup():
         paramPdfStorageDirectory_path = os.path.join(os.getcwd(), pathDatabase, "pdfDirectory") # A recupérer dans le frontend ? Donc pas ici mais dans "importFileWindow.py"
         paramOutputProcessingSteps_path = os.path.join(os.getcwd(), pathDatabase, "debug", "processingSteps") # A recupérer dans le frontend ? Donc pas ici mais dans "importFileWindow.py"
 
-        os.makedirs(paramPdfStorageDirectory_path, exist_ok=True) # Si on récupère ça du front alors logiquement, le dossier doit déjà existé 
-        os.makedirs(paramOutputProcessingSteps_path, exist_ok=True) # Si on récupère ça du front alors logiquement, le dossier doit déjà existé 
+        os.makedirs(paramPdfStorageDirectory_path, exist_ok=True) # We create the folder if it does not exist
+        os.makedirs(paramOutputProcessingSteps_path, exist_ok=True) # We create the folder if it does not exist
 
         processing = Processing(pdfDirectory_path=paramPdfStorageDirectory_path, outputProcessingSteps_path=paramOutputProcessingSteps_path)
         processing_thread = threading.Thread(target=processing.startProcessing)

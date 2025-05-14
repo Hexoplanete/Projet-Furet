c74c0b66f604184c056e57bdedd9e2d4e8b94557
tlevrard <thomaslevrardw@gmail.com>
2025-05-14 13:40:27 +0200

    code bouge de main à init de crawler

--- src/furet/crawler/__init__.py
+++ src/furet/crawler/__init__.py
@@ -2,6 +2,7 @@ import threading
 import time
 from furet.crawler.crawler import Crawler
 from furet import settings
+import os
 
 
 def setup():
@@ -20,7 +21,13 @@ def setup():
         end_time = time.time()
         print(f"Total execution time: {end_time - start_time:.2f} seconds")
 
-        traitement = Traitement()
-        traitement_thread = threading.Thread(target=traitement.startTraitement)
-        traitement_thread.start()
-        traitement_thread.join()
+        paramPdfStorageDirectory_path = os.path.join(os.getcwd(), "database", "pdfDirectory") # A recupérer dans le frontend ? Donc pas ici mais dans "importFileWindow.py"
+        paramOutputProcessingSteps_path = os.path.join(os.getcwd(), "database", "debug", "processingSteps") # A recupérer dans le frontend ? Donc pas ici mais dans "importFileWindow.py"
+
+        os.makedirs(paramPdfStorageDirectory_path, exist_ok=True) # Si on récupère ça du front alors logiquement, le dossier doit déjà existé 
+        os.makedirs(paramOutputProcessingSteps_path, exist_ok=True) # Si on récupère ça du front alors logiquement, le dossier doit déjà existé 
+
+        processing = Processing(pdfDirectory_path=paramPdfStorageDirectory_path, outputProcessingSteps_path=paramOutputProcessingSteps_path)
+        processing_thread = threading.Thread(target=processing.startProcessing)
+        processing_thread.start()
+        processing_thread.join()

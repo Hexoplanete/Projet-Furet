1d7da513ccee72bd3d9ba3a558d9d8e8bb00ed98
Spiky-73 <cocojeanne73@gmail.com>
2025-06-21 15:34:20 +0200

    WIP updater

---
+++ src/furet/updater/__init__.py
@@ -0,0 +1,27 @@
+
+import logging
+import subprocess
+
+logger = logging.getLogger("updater")
+
+def isInVersionMode():
+    gitTag = runCommand("git", "describe", "--tags", "--exact-match")
+    if gitTag.returncode != 0 or not gitTag.stdout.strip():
+        return False
+    gitBranch = runCommand("git", "branch", "--show-current")
+    if gitBranch.stdout.strip():
+        return False
+    return True
+
+def latestVersion():
+    _ = runCommand("git fetch --tags")
+    gitVersion = runCommand("git tag --sort=-creatordate | head -n 1", shell=True)
+    return gitVersion.stdout.strip()
+
+def updateToVersion(version: str):
+    _ = runCommand("git", "checkout", f"tags/{version}")
+
+
+def runCommand(*args, shell=False):
+    logger.debug(f"\"{' '.join(args)}\"")
+    return subprocess.run(args, shell=shell, capture_output=True, text=True)
